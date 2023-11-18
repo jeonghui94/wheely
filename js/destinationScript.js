490fe6b21b41d3451aef283988862e77e49b7b42
손정희 <com.wheely@gmail.com>
2023-11-18 22:42:08 +0900

    update

--- js/destinationScript.js
+++ js/destinationScript.js
@@ -1,32 +1,48 @@
 $(function () {
-    // $("#land3").css("fill", "#ff9621");
+    // 초기 설정
+    function init() {
+        $("#land4").css("fill", "#6885c5");
+        $("#tab4").css("display", "block");
+    }
+    init();
 
+    // 이벤트 설정
     $(".land").click(function () {
-        let land = $(this);
-        const idx = land.index() + 1;
-        $(".land").removeClass("on");
-        land.addClass("on");
-        $(".land_lnb").css("display", "none");
-        $(".land_lnb" + idx).css("display", "block");
+        mapAndLnbUpdate($(this));
+    });
+
+    $(".land_tag>ul>li").click(function () {
+        mapAndLnbUpdate($(this));
+    });
+
+    // 공통코드
+    function mapAndLnbUpdate(obj) {
+        const index = obj.index() + 1;
 
         // 지도 색 변경
-        // $(".land").css("fill", "#bcbcbc");
-        // switch (land.attr("id")) {
-        //     case "land1":
-        //         land.css("fill", "#ffd25a");
-        //         break;
-        //     case "land2":
-        //         land.css("fill", "#85cdf2");
-        //         break;
-        //     case "land3":
-        //         land.css("fill", "#ff9621");
-        //         break;
-        //     case "land4":
-        //         land.css("fill", "#6885c5");
-        //         break;
-        //     case "land5":
-        //         land.css("fill", "#529b58");
-        //         break;
-        // }
-    });
+        let fillValue;
+        switch (index) {
+            case 1:
+                fillValue = "#ffd25a";
+                break;
+            case 2:
+                fillValue = "#85cdf2";
+                break;
+            case 3:
+                fillValue = "#ff9621";
+                break;
+            case 4:
+                fillValue = "#6885c5";
+                break;
+            case 5:
+                fillValue = "#529b58";
+                break;
+        }
+        $(".land").removeAttr("style");
+        $("#land" + index).css("fill", fillValue);
+
+        // lnb(하위 카테고리) 변경
+        $(".land_lnb").removeAttr("style");
+        $("#tab" + index).css("display", "block");
+    }
 });
